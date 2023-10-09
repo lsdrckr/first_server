c3cf6fd8ff69e798801fe817c241ecc602e0c980
rduhr <Romain.Duhr@polytech-lille.net>
2023-10-09 22:30:58 +0200

    ajout_libreseau_09/10

---
+++ libs/Reseau/libreseau.h
@@ -0,0 +1,17 @@
+#ifndef __RESEAU_H__
+#define __RESEAU_H__
+
+int serverInit(char *service, int connections);
+/* Initialise le server sur le port du service et avec le nombre de connections spécifiées
+service : service utilisé parmis la liste des etc/services ex "tcp" => port 15
+connections : nombre de connections qui peuvent rester en queue avant d'être traitées
+*/
+
+int serverLoop(int sockFd, int (*traitement)(int));
+/* Attends une nouvelle connexion puis l'accepte et lance traitement pour le client
+sockFd : descripteur du socket
+traitement : nom de la fonction de traitement
+*/
+
+
+#endif
