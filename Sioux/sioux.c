3230bf7f8cd4ed02fe274ecf9a943070f3f7b3dc
lderycke <louis.deryckere@polytech-lille.net>
2023-10-10 10:13:58 +0100

    analyse_des_arguments_sious_10/10

--- Sioux/sioux.c
+++ Sioux/sioux.c
@@ -1,22 +1,49 @@
 #include <stdio.h>
 #include "libreseau.h"
 #include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+
+#define MAX_SERVICE_NAME 32
+#define DEFAULT_PORT 8080
+
+void analyzeArg(int argc, char* argv[], char service[]){
+    
+    int option;
+    
+    static struct option long_options[] = {
+        {"port", required_argument, NULL, 'p'}
+    };
+    while((option = getopt_long(argc, argv, "p:", long_options, NULL)) != -1){
+        switch(option) {
+            case 'p':
+                strcpy(service,optarg);
+                break;
+            default : 
+                perror("Option non reconnue");
+                exit(EXIT_FAILURE);
+        }
+    }
+}
 
 int clientGestion(){
     printf("Un nouveau client !\n");
     return 0;
 }
 
-int main(){
+int main(int argc, char* argv[]){
 
     int sockFd;
-
-    sockFd = serverInit("8080", 3);
+    char service[MAX_SERVICE_NAME] = "8080";
+    
+    analyzeArg(argc, argv, service);
+    
+    sockFd = serverInit(service, 3);
     if(sockFd < 0){
         perror("Port non utilisable\n");
         exit(EXIT_FAILURE);
     }
-    printf("Serveur initialisé\n");
+    printf("Serveur initialisé sur le port %s\n", service);
 
     printf("Serveur à l'écoute ...\n");
     serverLoop(sockFd, clientGestion);
